eae37fbdc016b2b1dd5aa1f155dfe42b34efb852
YoussefMennani <you@example.comyoussefmennani1996@gmail.com>
2023-11-24 14:48:25 +0100

    1st

---
+++ app/src/main/java/org/iao/jumia/jumia.java
@@ -0,0 +1,14 @@
+package org.iao.jumia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jumia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jumia);
+    }
+}
